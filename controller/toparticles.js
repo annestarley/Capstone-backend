1f4546335da09776956fc42aa68efb540061c42b
Anne Starley <annestarley@gmail.com>
2018-04-03 17:19:10 -0700

    Working on a practice example of one, but not working on a practice example of many. It does takes some time, maybe move it down to 10 instead of 20.

--- controller/toparticles.js
+++ controller/toparticles.js
@@ -1,6 +1,4 @@
 const model = require('../models/toparticles')
-const axios = require('axios')
-const read = require('node-readability')
 
 const toparticlesController = (req, response, next) => {
   model.getTopArticles()
--- scraps.js
+++ scraps.js
@@ -49,3 +49,198 @@
     .catch(err => {
       console.log(err);
     })
+
+
+
+    axios({
+      method: 'post',
+      url: 'https://gateway.watsonplatform.net/tone-analyzer/api/v3/tone?version=2016-05-19',
+      data: data,
+      auth: {
+        username: '8efacbc7-5172-4acb-960c-9c5ed3ff10e4',
+        password: 'ODefuwdWJxeJ'
+      }
+    })
+    .then(res => {
+      let emotionTone = res.data.document_tone.tone_categories[0].tones;
+      console.log(emotionTone)
+      let languageTone = res.data.document_tone.tone_categories[1].tones
+      console.log(languageTone);
+      let socialTone = res.data.document_tone.tone_categories[2].tones
+      console.log(socialTone)
+      let anger = emotionTone[0].score
+      console.log('ANGER!!!', anger)
+      let disgust = emotionTone[1].score
+      console.log('DISGUST!!', disgust)
+      let fear = emotionTone[2].score
+      console.log('FEAR!!!', fear)
+      let joy = emotionTone[3].score
+      console.log('JOY!!!', joy)
+      let analytical = languageTone[0].score
+      console.log('ANALYTICAL!!', analytical);
+      let confident = languageTone[1].score
+      console.log('CONFIDENT!', confident);
+      let tentative = languageTone[2].score
+      console.log('TENTATIVE', tentative);
+      let openness = socialTone[0].score
+      console.log('OPEN', openness);
+      let conscientiousness = socialTone[1].score
+      console.log('SOCIAL TONE', socialTone);
+      let extraversion = socialTone[2].score
+      console.log('EXTRAVERSION', extraversion);
+      let agreeableness = socialTone[3].score
+      console.log('AGREEABLENESS', agreeableness)
+      let emotionalRange = socialTone[4].score
+      console.log('EMOTIONAL RANGE', emotionalRange)
+    })
+    .catch(err => {
+      console.log(err);
+    })
+
+
+
+
+
+    axios.get(`https://newsapi.org/v2/top-headlines?country=us&apiKey=a4d490bdd93d405d8bc422ace61c795f`)
+      .then(res => {
+        let results = []
+        let anger
+        let disgust
+        let fear
+        let joy
+        let sadness
+        let analytical
+        let confident
+        let tentative
+        let openness
+        let conscientiousness
+        let extraversion
+        let agreeableness
+        let emotionalRange
+        let article =res.data.articles[0]
+          let url = article.url
+          read(url, function(err, article, meta) {
+            // console.log(article.content);
+            // console.log('textBody!!!!!!!!--------------', article.textBody);
+            axios({
+              method: 'post',
+              url: 'https://gateway.watsonplatform.net/tone-analyzer/api/v3/tone?version=2016-05-19',
+              data: { "text": article.textBody},
+              auth: {
+                username: '8efacbc7-5172-4acb-960c-9c5ed3ff10e4',
+                password: 'ODefuwdWJxeJ'
+              }
+            })
+            .then(res => {
+              let emotionTone = res.data.document_tone.tone_categories[0].tones;
+              console.log(emotionTone)
+              let languageTone = res.data.document_tone.tone_categories[1].tones
+              console.log(languageTone);
+              let socialTone = res.data.document_tone.tone_categories[2].tones
+              console.log(socialTone)
+              anger = emotionTone[0].score
+              console.log('ANGER!!!', anger)
+              disgust = emotionTone[1].score
+              console.log('DISGUST!!', disgust)
+              fear = emotionTone[2].score
+              console.log('FEAR!!!', fear)
+              joy = emotionTone[3].score
+              console.log('JOY!!!', joy)
+              analytical = languageTone[0].score
+              console.log('ANALYTICAL!!', analytical);
+              confident = languageTone[1].score
+              console.log('CONFIDENT!', confident);
+              tentative = languageTone[2].score
+              console.log('TENTATIVE', tentative);
+              openness = socialTone[0].score
+              console.log('OPEN', openness);
+              conscientiousness = socialTone[1].score
+              console.log('SOCIAL TONE', socialTone);
+              extraversion = socialTone[2].score
+              console.log('EXTRAVERSION', extraversion);
+              agreeableness = socialTone[3].score
+              console.log('AGREEABLENESS', agreeableness)
+              emotionalRange = socialTone[4].score
+              console.log('EMOTIONAL RANGE', emotionalRange)
+            })
+            .catch(err => {
+              console.log(err);
+            })
+            article.close();
+          });
+          let obj = {
+            url: article.url,
+            title: article.title,
+            description: article.description,
+            source: article.source.name,
+            author: article.author,
+            image: article.urlToImage,
+            tones: {
+              emotion: {
+                anger,
+                disgust,
+                fear,
+                joy
+              },
+              language: {
+                analytical,
+                confident,
+                tentative
+              },
+              social: {
+                openness,
+                conscientiousness,
+                extraversion,
+                agreeableness,
+                emotionalRange
+              }
+            }
+          }
+          console.log(obj)
+          results.push(obj)
+
+        console.log(results)
+      })
+      .catch(err => {
+        console.log(err);
+      })
+
+
+
+
+
+      let obj = {
+        url: article.url,
+        title: article.title,
+        description: article.description,
+        source: article.source.name,
+        author: article.author,
+        image: article.urlToImage,
+        tones: {
+          emotion: {
+            anger: anger || '0.0',
+            disgust: disgust || '0.0',
+            fear: fear || '0.0',
+            joy: joy || '0.0'
+          },
+          language: {
+            analytical: analytical || '0.0',
+            confident: confident || '0.0',
+            tentative: tentative || '0.0'
+          },
+          social: {
+            openness: openness || '0.0',
+            conscientiousness: conscientiousness || '0.0',
+            extraversion: extraversion || '0.0',
+            agreeableness: agreeableness || '0.0',
+            emotionalRange: emotionalRange || '0.0'
+          }
+        }
+      }
+      console.log('anger', anger)
+      console.log('fear', fear)
+      console.log('joy', joy)
+      console.log(obj)
+      results.push(obj)
+
+    console.log(results)
